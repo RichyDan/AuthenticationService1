cf19cf4a18d0f9f76519a66658d0c962fc80ee68
Daniil <rychenkov16@yandex.ru>
2025-04-09 23:03:25 +0300

    1

--- AuthenticationService1/Controllers/UserController.cs
+++ AuthenticationService1/Controllers/UserController.cs
@@ -1,18 +1,33 @@
 ﻿
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace AuthenticationService.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class UserController : ControllerBase
     {
-        [ApiController]
-        [Route("[controller]")]
         public UserController()
         {
             var logger = new Logger();
 
             logger.WriteEvent("Сообщение о событии в программе");
-            logger.WriteError("Сообщение об ошибки в программе");
+            logger.WriteError("Сообщение об ошибке в программе");
+        }
+
+        [HttpGet]
+        public User GetUser()
+        {
+            return new User()
+            {
+                Id = Guid.NewGuid(),
+                Login = "ivanov",
+                Password = "11111111",
+                FirstName = "Иван",
+                LastName = "Иванов",
+                Email = "example@gmail.com"
+            };
         }
     }
 }
