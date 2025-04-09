dee5e48977492c9519fb7d2fdcb1b6b2668be498
Daniil <rychenkov16@yandex.ru>
2025-04-09 22:51:16 +0300

    Add project files.

---
+++ AuthenticationService1/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationService.Controllers
+{
+    public class UserController : ControllerBase
+    {
+        [ApiController]
+        [Route("[controller]")]
+        public UserController()
+        {
+            var logger = new Logger();
+
+            logger.WriteEvent("Сообщение о событии в программе");
+            logger.WriteError("Сообщение об ошибки в программе");
+        }
+    }
+}
